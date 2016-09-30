16105b46fc17823c53b57bc55538491a3c024c0a
Roni Choudhury <roni.choudhury@kitware.com>
2016-09-30 12:45:45 -0400

    Subscribe to application to dump state to console

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { select } from 'd3-selection';
 
 import page from './template/page.jade';
+import { observeStore } from './store';
+
+const report = (state) => {
+  console.log('[report]');
+  console.log(state.toJS());
+}
 
 window.onload = () => {
   select(document.body)
     .html(page());
+
+  observeStore(report);
 };
