76022f9f7796c3cb87692b54d1b437b32953c166
Roni Choudhury <roni.choudhury@kitware.com>
2016-11-04 12:45:01 -0400

    Abstract action type and constructor creation

--- src/action/index.js
+++ src/action/index.js
@@ -1,26 +1,47 @@
 import { makeEnum } from '../util';
 
-const actionType = makeEnum('actionType', [
-  'switchMode',
-  'saving',
-  'changeName'
-]);
+const actionSpec = {
+  switchMode: ['mode'],
+  saving: [],
+  changeName: ['name']
+};
 
-const action = Object.freeze({
-  switchMode: (mode) => ({
-    type: actionType.switchMode,
-    mode
-  }),
+const buildActionConstructor = (type, argSpec) => {
+  if (argSpec.some(v => v === 'type')) {
+    throw new Error('fatal: action constructor for "${type}" has an argument named "type"');
+  }
 
-  saving: () => ({
-    type: actionType.saving
-  }),
+  return (...args) => {
+    let action = {
+      type
+    };
+    for (let i = 0; i < argSpec.length; i++) {
+      action[argSpec[i]] = args[i];
+    }
+    return action;
+  }
+};
 
-  changeName: (name) => ({
-    type: actionType.changeName,
-    name
-  })
-});
+const buildActions = () => {
+  let action = {};
+  let actionType = makeEnum('actionType', Object.keys(actionSpec));
+
+  for(let key in actionSpec) {
+    if (actionSpec.hasOwnProperty(key)) {
+      action[key] = buildActionConstructor(actionType[key], actionSpec[key]);
+    }
+  }
+
+  return {
+    actionType,
+    action
+  };
+};
+
+const {
+  actionType,
+  action
+} = buildActions();
 
 export {
   actionType,
